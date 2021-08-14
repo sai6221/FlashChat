193c36e36d120ff0f3c59b8d7fcba5367dbf7ce7
Sai Reddy <sai6221@gmail.com>
2021-08-14 14:48:15 +0530

    Cocoapods added

--- FlashChat/Controller/ViewController.swift
+++ FlashChat/Controller/ViewController.swift
@@ -6,23 +6,26 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = "⚡️FlashChat"
         // Do any additional setup after loading the view.
-        let title = "⚡️FlashChat"
-        var charIndex = 0.0
-        titleLabel.text = ""
-        for i in title{
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
-                self.titleLabel.text?.append(i)
-            }
-            charIndex += 1
-        }
+//        let title = "⚡️FlashChat"
+//        var charIndex = 0.0
+//        titleLabel.text = ""
+//        for i in title{
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { timer in
+//                self.titleLabel.text?.append(i)
+//            }
+//            charIndex += 1
+//        }
     }
 
 
